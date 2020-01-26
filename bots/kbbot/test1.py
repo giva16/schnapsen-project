d6aaa7b15e2e861e0c5d56ba4b80c7518de4b912
giva16 <ltpie1@hotmail.com>
2020-01-26 06:59:02 +0100

    editing test4.py

--- bots/kbbot/test1.py
+++ bots/kbbot/test1.py
@@ -2,18 +2,19 @@ import sys
 from kb import KB, Boolean, Integer, Constant
 
 # Define our symbols
-A = Boolean('A')
-B = Boolean('B')
-C = Boolean('C')
+Q = Boolean('Q')
+P = Boolean('P')
+R = Boolean('R')
 
 # Create a new knowledge base
 kb = KB()
 
 # Add clauses
-kb.add_clause(A, B)
-kb.add_clause(~B, A)
-kb.add_clause(~A. C)
-kb.add_clause(~A, D)
+kb.add_clause(P, Q)
+kb.add_clause(~Q, R)
+kb.add_clause(~R, ~P)
+kb.add_clause(Q, ~P)
+kb.add_clause(P,~Q)
 
 # Print all models of the knowledge base
 for model in kb.models():
